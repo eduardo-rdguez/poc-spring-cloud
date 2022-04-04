ccacea4e2875bd0fd643babab706509ddc78bd34
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-03 23:27:13 -0500

    Add eureka server

---
+++ eureka-server/src/test/java/rdguez/eduardo/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rdguez.eduardo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
